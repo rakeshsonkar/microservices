7b3e3845b14b4e6c72bf0b628546f44d92806526
rakeshsonkar <rksonkar356@gmail.com>
2022-11-29 10:31:36 +0530

    script sql

--- rating/src/main/java/com/rating/entities/Rating.java
+++ rating/src/main/java/com/rating/entities/Rating.java
@@ -1,11 +1,12 @@
 package com.rating.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.Data;
 
 @Data
-@
+@Document("Rating_Micro")
 public class Rating {
 
 	@Id
---
+++ sql-scripts/ifms.sql
@@ -0,0 +1,7 @@
+show user;
+
+create table abc ( 
+Id INT ,Name varchar(50),city VARCHAR(50)
+);
+DESCRIBE abc;
+select * from abc;
